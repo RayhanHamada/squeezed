d4c653d4fb70b777200bb8c9eeb42536980a029c
RayhanHamada <hamadadev2016@gmail.com>
2021-07-09 01:05:45 +0700

    feat(web-client): improve dashboard

--- packages/web-client/pages/dashboard.tsx
+++ packages/web-client/pages/dashboard.tsx
@@ -1,6 +1,7 @@
 import { CreateLinkDrawer } from '@/components/CreateLinkDrawer';
 import { Navbar } from '@/components/Navbar';
 import { Redirecting } from '@/components/Redirecting';
+import { dayjs } from '@/lib/dayjs';
 import { fb } from '@/lib/firebase-client';
 import { URLData } from '@/lib/model-types';
 import { useUserDataStore } from '@/lib/store';
@@ -9,10 +10,11 @@ import {
   Button,
   Checkbox,
   Container,
-  Divider,
   Flex,
   Skeleton,
   Spacer,
+  StackDivider,
+  Switch,
   Text,
   useDisclosure,
   VStack,
@@ -20,7 +22,7 @@ import {
 import { GetServerSideProps } from 'next';
 import { useRouter } from 'next/dist/client/router';
 import Head from 'next/head';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { useCollection } from 'react-firebase-hooks/firestore';
 import { FaPlus } from 'react-icons/fa';
@@ -29,6 +31,8 @@ export default function Dashboard() {
   const router = useRouter();
   const { uid } = useUserDataStore();
   const [user, isAuthLoading] = useAuthState(fb.auth());
+
+  const [selectedID, setSelectedID] = useState('');
 
   /**
    * Create Link Modal disclosure
@@ -42,7 +46,7 @@ export default function Dashboard() {
   /**
    * ambil semua url_data dari firestore
    */
-  const [snapshot, , isCollectionLoading] = useCollection(
+  const [snapshot, isCollectionLoading, error] = useCollection(
     fb.firestore().collection('url_data').where('uid', '==', uid)
   );
 
@@ -69,7 +73,7 @@ export default function Dashboard() {
   return (
     <Container
       maxW="full"
-      h={{ base: '120vh', md: 'full', lg: '100vh' }}
+      h={{ base: '120vh', md: '100vh', lg: '100vh' }}
       p="0"
       marginX="0"
       bgColor="black"
@@ -85,7 +89,7 @@ export default function Dashboard() {
         onClose={onCreateLinkModalClose}
       />
       <Container maxW="full" pt="8">
-        <Flex flexDir="column" px="4" h="50vh" maxH="full">
+        <Flex flexDir="column" px="4" h="80vh" maxH="full">
           <Flex>
             <Text textColor="white" fontSize="2xl" fontWeight="bold">
               Your Links
@@ -98,7 +102,7 @@ export default function Dashboard() {
               </Button>
             </Skeleton>
           </Flex>
-          <Box h="4" />
+          <Box h="8" />
           <Skeleton isLoaded={!isCollectionLoading}>
             {!snapshot || snapshot?.docs.length === 0 ? (
               <Text textColor="white" textAlign="center">
@@ -106,32 +110,89 @@ export default function Dashboard() {
                 the right.
               </Text>
             ) : (
-              <VStack overflowY="auto" h="40vh">
+              <VStack
+                overflowY="auto"
+                spacing="4"
+                h="40vh"
+                w="full"
+                align="start"
+                divider={<StackDivider borderColor="gray.200" />}
+              >
                 {snapshot.docs.map((doc) => {
-                  const urlData = {
+                  const {
+                    id,
+                    title,
+                    created_at,
+                    uuid_code,
+                    expire_at,
+                    enabled,
+                  } = {
                     id: doc.ref.id,
                     ...doc.data(),
                   } as URLData;
 
+                  const dateCreatedAt = dayjs
+                    .unix(created_at)
+                    .format('DD/MM/YYYY HH:mm');
+
+                  const dateExpiredAt = expire_at
+                    ? dayjs.unix(expire_at).format('DD/MM/YYYY HH:mm')
+                    : undefined;
+
                   return (
                     <Flex
-                      key={urlData.id}
-                      justifyContent="flex-start"
+                      key={id}
+                      w="full"
+                      justifyContent="start"
                       alignItems="center"
+                      opacity={id === selectedID ? 0.7 : 1}
+                      pr="4"
                     >
-                      <Checkbox colorScheme="green" />
+                      <Box>
+                        <Checkbox colorScheme="green" />
+                      </Box>
+                      <VStack alignItems="start" px="4">
+                        <Text
+                          color="white"
+                          fontWeight="bold"
+                          fontSize="md"
+                          onClick={() => setSelectedID(id!)}
+                          _hover={{
+                            cursor: 'pointer',
+                            textDecor: 'underline',
+                          }}
+                        >
+                          {title}
+                        </Text>
+                        <Button color="white" variant="link" fontSize="sm">
+                          sqzd.xyz/{uuid_code}
+                        </Button>
+                        <Text color="white" fontSize="xs">
+                          Created at: {dateCreatedAt} <b> | </b>
+                          {dateExpiredAt
+                            ? ` Expired at ${dateExpiredAt}`
+                            : 'Permanent'}
+                        </Text>
+                      </VStack>
+                      <Spacer />
+                      <Text color="white" pr="2">
+                        Enabled
+                      </Text>
+                      <Switch
+                        isChecked={enabled}
+                        onChange={(e) => {
+                          e.preventDefault();
+                          doc.ref.update({
+                            enabled: !enabled,
+                          } as Partial<URLData>);
+                        }}
+                      ></Switch>
                     </Flex>
                   );
                 })}
               </VStack>
             )}
           </Skeleton>
-        </Flex>
-        <Divider color="white" />
-        <Flex flexDir="column" maxH="full" h="full" px="4" pt="4">
-          <Text textColor="white" fontSize="2xl" fontWeight="bold">
-            Link Title
-          </Text>
         </Flex>
       </Container>
     </Container>
